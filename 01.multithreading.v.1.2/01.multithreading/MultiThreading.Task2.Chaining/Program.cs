0881d447f3bf1cd8ec513998afda19c0d56fac13
EgorMishchenko <prickpunk@yandex.ru>
2019-10-01 09:24:30 +0300

    Initial commit.

---
+++ 01.multithreading.v.1.2/01.multithreading/MultiThreading.Task2.Chaining/Program.cs
@@ -0,0 +1,90 @@
+﻿/*
+ * 2.	Write a program, which creates a chain of four Tasks.
+ * First Task – creates an array of 10 random integer.
+ * Second Task – multiplies this array with another random integer.
+ * Third Task – sorts this array by ascending.
+ * Fourth Task – calculates the average value. All this tasks should print the values to console.
+ */
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultiThreading.Task2.Chaining
+{
+    class Program
+    {
+        public const int ArraySize = 10;
+
+        private static int[] NumberArray { get; set; }
+        public static Random Random { get; set; }
+
+        static Program()
+        {
+            NumberArray = new int[ArraySize];
+            Random = new Random();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".Net Mentoring Program. MultiThreading V1 ");
+            Console.WriteLine("2.	Write a program, which creates a chain of four Tasks.");
+            Console.WriteLine("First Task – creates an array of 10 random integer.");
+            Console.WriteLine("Second Task – multiplies this array with another random integer.");
+            Console.WriteLine("Third Task – sorts this array by ascending.");
+            Console.WriteLine("Fourth Task – calculates the average value. All this tasks should print the values to console");
+            Console.WriteLine();
+
+            var task = new Task(FillArrayWithRandomNumbers);
+
+            task.ContinueWith(MultiplyArray)
+                .ContinueWith(SortArray)
+                .ContinueWith(CalculateAverage);
+
+            task.Start();
+
+            Console.ReadLine();
+        }
+
+        static void FillArrayWithRandomNumbers()
+        {
+            Console.WriteLine("Filled array");
+            for (int i = 0; i < NumberArray.Length; i++)
+            {
+                NumberArray[i] = Random.Next(0, 100);
+                Console.WriteLine(NumberArray[i]);
+            }
+        }
+
+        static void MultiplyArray(Task task)
+        {
+            var randomNumber = Random.Next(0, 100);
+            Console.WriteLine($"Multiplied array by {randomNumber}:");
+            
+            for (int i = 0; i < NumberArray.Length; i++)
+            {
+                NumberArray[i] *= randomNumber;
+                Console.WriteLine(NumberArray[i]);
+            }
+        }
+
+        static void SortArray(Task task)
+        {
+            Console.WriteLine("Sorted array:");
+            Array.Sort(NumberArray);
+
+            foreach (var number in NumberArray)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        static void CalculateAverage(Task task)
+        {
+            Console.WriteLine("Average:");
+            var average = Enumerable.Average(NumberArray);
+            Console.WriteLine(average);
+        }
+    }
+}
---
+++ 01.multithreading.v.1.2/01.multithreading/MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * 5. Write a program which creates two threads and a shared collection:
+ * the first one should add 10 elements into the collection and the second should print all elements
+ * in the collection after each adding.
+ * Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.
+ */
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace MultiThreading.Task5.Threads.SharedCollection
+{
+    class Program
+    {
+        static object locker = new object();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
+            Console.WriteLine("the first one should add 10 elements into the collection and" +
+                              " the second should print all elements in the collection after each adding.");
+            Console.WriteLine("Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.");
+            Console.WriteLine();
+
+            var sharedCollection = new List<int>();
+
+            var thread1 = new Thread(new ParameterizedThreadStart(AddElementsToCollection));
+
+            thread1.Start(sharedCollection);
+
+            Console.ReadLine();
+        }
+
+        static void AddElementsToCollection(object objectList)
+        {
+            lock (locker)
+            {
+                var random = new Random();
+
+                var list = (ICollection<int>)objectList;
+
+                for (int i = 0; i < 5; i++)
+                {
+                    list.Add(random.Next(0, 100));
+                    PrintAllElements(list);
+                }
+            }
+        }
+
+        static void PrintAllElements(object objectList)
+        {
+            var t = new Thread(() =>
+            {
+                lock (locker)
+                {
+                    var list = (ICollection<int>)objectList;
+
+                    Console.WriteLine("All elements: ");
+                    foreach (var number in list)
+                    {
+                        Console.WriteLine(number);
+                    }
+                }
+            });
+
+            t.Start();
+        }
+    }
+}
