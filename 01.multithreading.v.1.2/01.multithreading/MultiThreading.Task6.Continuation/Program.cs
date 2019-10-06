0c5841af2202c35a7e5e01cf247885de3ef5cbed
EgorMishchenko <prickpunk@yandex.ru>
2019-10-06 12:44:46 +0300

    6th task, not completed

--- 01.multithreading.v.1.2/01.multithreading/MultiThreading.Task6.Continuation/Program.cs
+++ 01.multithreading.v.1.2/01.multithreading/MultiThreading.Task6.Continuation/Program.cs
@@ -7,6 +7,10 @@
    Demonstrate the work of the each case with console utility.
 */
 using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task6.Continuation
 {
@@ -22,9 +26,65 @@ namespace MultiThreading.Task6.Continuation
             Console.WriteLine("Demonstrate the work of the each case with console utility.");
             Console.WriteLine();
 
-            // feel free to add your code
+            var parentTask = Task.Run(() =>
+            {
+                Console.WriteLine($"Parent thread id: {Thread.CurrentThread.ManagedThreadId}");
+                Console.WriteLine("Parent method executing");
+
+                throw new ArgumentException("Task failed");
+            });
+
+            parentTask.ContinueWith(IndependentMethod);
+
+            parentTask.ContinueWith(WhenParentFailMethod, TaskContinuationOptions.OnlyOnFaulted);
+
+            parentTask.ContinueWith(WhenParentFailAndUsingParentThreadMethod, TaskScheduler.FromCurrentSynchronizationContext());
+
+            //var taskD = parentTask.ContinueWith();
 
             Console.ReadLine();
         }
+
+        private static void ParentMethod()
+        {
+            Console.WriteLine($"Parent thread id: {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine("Parent method executing");
+
+        }
+
+        private static void IndependentMethod(Task task)
+        {
+            Console.WriteLine("Starting independent task:");
+            Console.WriteLine("...task should be executed regardless of the result of the parent task");
+            Console.WriteLine();
+        }
+
+        private static void WhenParentFailMethod(Task task)
+        {
+            Console.WriteLine("Starting continuation after failed parent task:");
+            Console.WriteLine("...task should be executed when the parent task finished without success.");
+            Console.WriteLine();
+        }
+
+        private static void WhenParentFailAndUsingParentThreadMethod(Task task)
+        {
+            if (task.IsFaulted)
+            {
+                Console.WriteLine($"Thread id: {Thread.CurrentThread.ManagedThreadId}");
+                Console.WriteLine("Starting continuation after failed parent task and using parent thread:");
+                Console.WriteLine("...task should be executed when the parent task would be finished with fail " +
+                                  "and parent task thread should be reused for continuation.");
+                Console.WriteLine();
+            }
+        }
+
+        private static void OutsideThreadPoolMethod()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Starting continuation after failed parent task:");
+            Console.WriteLine("...task should be executed outside of the thread pool when the parent task would be cancelled.");
+            Console.WriteLine();
+        }
+    }
     }
 }
