ae7c62f99f07d65c5b646bf68bc13250578d79a8
EgorMishchenko <prickpunk@yandex.ru>
2019-10-08 00:50:24 +0300

    6th task from multithreading.

--- 01.multithreading.v.1.2/01.multithreading/MultiThreading.Task6.Continuation/Program.cs
+++ 01.multithreading.v.1.2/01.multithreading/MultiThreading.Task6.Continuation/Program.cs
@@ -8,6 +8,7 @@
 */
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
@@ -19,37 +20,66 @@ namespace MultiThreading.Task6.Continuation
         static void Main(string[] args)
         {
             Console.WriteLine("Create a Task and attach continuations to it according to the following criteria:");
-            Console.WriteLine("a.    Continuation task should be executed regardless of the result of the parent task.");
-            Console.WriteLine("b.    Continuation task should be executed when the parent task finished without success.");
-            Console.WriteLine("c.    Continuation task should be executed when the parent task would be finished with fail and parent task thread should be reused for continuation.");
-            Console.WriteLine("d.    Continuation task should be executed outside of the thread pool when the parent task would be cancelled.");
+            Console.WriteLine(
+                "a.    Continuation task should be executed regardless of the result of the parent task.");
+            Console.WriteLine(
+                "b.    Continuation task should be executed when the parent task finished without success.");
+            Console.WriteLine(
+                "c.    Continuation task should be executed when the parent task would be finished with fail and parent task thread should be reused for continuation.");
+            Console.WriteLine(
+                "d.    Continuation task should be executed outside of the thread pool when the parent task would be cancelled.");
             Console.WriteLine("Demonstrate the work of the each case with console utility.");
             Console.WriteLine();
 
-            var parentTask = Task.Run(() =>
+            Console.WriteLine("DEMONSTRATION");
+            // a.
+            var parentTaskForIndependentContinue = Task.Run(() =>
             {
                 Console.WriteLine($"Parent thread id: {Thread.CurrentThread.ManagedThreadId}");
                 Console.WriteLine("Parent method executing");
-
-                throw new ArgumentException("Task failed");
             });
 
-            parentTask.ContinueWith(IndependentMethod);
+            parentTaskForIndependentContinue.ContinueWith(IndependentMethod);
 
-            parentTask.ContinueWith(WhenParentFailMethod, TaskContinuationOptions.OnlyOnFaulted);
+            // b.
+            Task.Run(() => throw new CustomException("some exception"))
+                .ContinueWith(t => { Console.WriteLine($"{t.Exception.Message}"); },
+                    TaskContinuationOptions.OnlyOnFaulted);
 
-            parentTask.ContinueWith(WhenParentFailAndUsingParentThreadMethod, TaskScheduler.FromCurrentSynchronizationContext());
+            // c.
+            Task.Run(() =>
+                {
+                    Console.WriteLine($"Parent thread ID: {Thread.CurrentThread.ManagedThreadId}");
+                    throw new CustomException("some exception");
+                }).ContinueWith(t =>
+            {
 
-            //var taskD = parentTask.ContinueWith();
+                Console.WriteLine($"Children thread ID: {Thread.CurrentThread.ManagedThreadId}");
+                Console.WriteLine($"{t.Exception.Message}");
+
+            }, CancellationToken.None, TaskContinuationOptions.OnlyOnFaulted, TaskScheduler.Current);
+
+           // d/
+            CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancelTokenSource.Token;
+
+            var task = Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        token.ThrowIfCancellationRequested();
+                        Console.WriteLine("parent task is working, press any to cancel it....");
+                        Thread.Sleep(1000);
+                    }
+                }, token)
+                .ContinueWith(AfterAntecedentTaskWasCancelledMethod, 
+                    TaskContinuationOptions.OnlyOnCanceled);
 
             Console.ReadLine();
-        }
+            cancelTokenSource.Cancel();
+            task.Wait();
 
-        private static void ParentMethod()
-        {
-            Console.WriteLine($"Parent thread id: {Thread.CurrentThread.ManagedThreadId}");
-            Console.WriteLine("Parent method executing");
-
+            Console.ReadLine();
         }
 
         private static void IndependentMethod(Task task)
@@ -59,32 +89,17 @@ namespace MultiThreading.Task6.Continuation
             Console.WriteLine();
         }
 
-        private static void WhenParentFailMethod(Task task)
+        private static void AfterAntecedentTaskWasCancelledMethod(Task task)
         {
-            Console.WriteLine("Starting continuation after failed parent task:");
-            Console.WriteLine("...task should be executed when the parent task finished without success.");
-            Console.WriteLine();
-        }
-
-        private static void WhenParentFailAndUsingParentThreadMethod(Task task)
-        {
-            if (task.IsFaulted)
-            {
-                Console.WriteLine($"Thread id: {Thread.CurrentThread.ManagedThreadId}");
-                Console.WriteLine("Starting continuation after failed parent task and using parent thread:");
-                Console.WriteLine("...task should be executed when the parent task would be finished with fail " +
-                                  "and parent task thread should be reused for continuation.");
-                Console.WriteLine();
-            }
-        }
-
-        private static void OutsideThreadPoolMethod()
-        {
-            Console.WriteLine();
-            Console.WriteLine("Starting continuation after failed parent task:");
+            Console.WriteLine($"Children task status: {task.Status}");
+            Console.WriteLine("Starting after parent task was cancelled:");
             Console.WriteLine("...task should be executed outside of the thread pool when the parent task would be cancelled.");
-            Console.WriteLine();
         }
     }
+
+    public class CustomException : Exception
+    {
+        public CustomException(String message) : base(message)
+        { }
     }
 }
