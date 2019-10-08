8197da5873ab6785799da30253d94526698a9fe8
Egor Mishchenko <Egor_Mishchenko@epam.com>
2019-10-08 10:11:09 +0300

    Add async solution for shared collection, some small fixes.

---
+++ 01.multithreading.v.1.2/01.multithreading/MultiThreading.Task5.Threads.SharedCollection/SharedCollectionByAsync.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreading.Task5.Threads.SharedCollection
+{
+  public sealed class SharedCollectionByAsync
+  {
+    public int ArraySize { get; set; }
+    private List<int> SharedCollection { get; }
+
+    public SharedCollectionByAsync(int arraySize)
+    {
+      ArraySize = arraySize;
+      SharedCollection = new List<int>();
+    }
+    public void FillCollectionWithRandomElements()
+    {
+      var thread = new Thread(async () =>
+      {
+        var random = new Random();
+
+        for (int i = 0; i < ArraySize; i++)
+        {
+          SharedCollection.Add(random.Next(0, 10));
+          await PrintAllElementsAsync();
+        }
+      });
+
+      thread.Start();
+    }
+
+    private Task PrintAllElementsAsync()
+    {
+      Console.WriteLine("All elements: ");
+
+      foreach (var element in SharedCollection)
+      {
+        Console.WriteLine(element);
+      }
+
+      Console.WriteLine($"Total count: {SharedCollection.Count}");
+      Console.WriteLine();
+
+      return Task.CompletedTask;
+    }
+  }
+}
--- 01.multithreading.v.1.2/01.multithreading/MultiThreading.Task6.Continuation/Program.cs
+++ 01.multithreading.v.1.2/01.multithreading/MultiThreading.Task6.Continuation/Program.cs
@@ -101,5 +101,4 @@ namespace MultiThreading.Task6.Continuation
     {
         public CustomException(String message) : base(message)
         { }
-    }
 }
